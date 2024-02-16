ea04d3e7eb15c13a7492b245c892035fe17c807e
Mika <mikush.carmel@gmail.com>
2024-02-16 21:11:07 +0200

    feat(addRedux): added redux on select match

--- src/components/selectFromData/selectFromData.tsx
+++ src/components/selectFromData/selectFromData.tsx
@@ -3,10 +3,19 @@ import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import useStyles from "./selectFromDataStyles";
+import { useAppSelector } from "../../redux/hooks";
+import { SelectMatchState } from "../../types/interfaces";
+import { useDispatch } from "react-redux";
+import { setMatchTeam } from "../../redux/matchDataSlice";
 
 const SelectFromData: React.FC<SelectFromDataProps> = (props) => {
   const { classes } = useStyles();
-  const { name, data, chosen, setChosen, dataTranslate } = props;
+  const { name, data, dataTranslate } = props;
+
+  const chosen = useAppSelector(
+    (state) => state[name as keyof SelectMatchState]
+  );
+  const dispatch = useDispatch();
 
   return (
     <FormControl className={classes.selectBox}>
@@ -15,7 +24,9 @@ const SelectFromData: React.FC<SelectFromDataProps> = (props) => {
         labelId={name + "-label"}
         value={chosen}
         label={name}
-        onChange={(e) => setChosen(e.target.value)}
+        onChange={(e) => {
+          dispatch(setMatchTeam({ name: name, input: e.target.value }));
+        }}
       >
         {data.map((item, index) => (
           <MenuItem key={index} value={item}>
--- src/components/selectFromData/selectFromDataProps.ts
+++ src/components/selectFromData/selectFromDataProps.ts
@@ -1,7 +1,5 @@
 interface SelectFromDataProps {
     name: string;
     data: string[];
-    chosen: string;
-    setChosen: React.Dispatch<React.SetStateAction<string>>;
     dataTranslate: (key: string, index? : number) => string;
 }
