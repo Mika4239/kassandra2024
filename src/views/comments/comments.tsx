9a58d685c5f1febc4285c4e4e81c39935846af22
Mika <mikush.carmel@gmail.com>
2024-02-16 19:49:12 +0200

    feat(addRedux): added comments for pentalties and other

--- src/views/comments/comments.tsx
+++ src/views/comments/comments.tsx
@@ -1,12 +1,14 @@
-import { TextField } from "@mui/material";
 import useStyles from "./commentsStyles";
 import NavigationButtons from "../../components/navigationButtons/navigationButtons";
+import { Textarea } from "@mui/joy";
 
 const PREV_PATH = 'endgame';
 const NEXT_PATH = 'select';
 
 const COMMENTS_TITLE = 'Comments';
 const DEFENCE_TITLE = 'Defence';
+const PENALTIES_TITLE = 'Penalties';
+const OTHER_TITLE = 'Other';
 
 const Comments: React.FC = () => {
   const { classes } = useStyles();
@@ -15,7 +17,11 @@ const Comments: React.FC = () => {
     <div className={classes.commentsPage}>
       <h1 className={classes.mainTitle}>{COMMENTS_TITLE}</h1>
       <h2 className={classes.subTitle}>{DEFENCE_TITLE}</h2>
-      <TextField />
+      <Textarea size='lg' minRows={5} style={{width: '30%'}}/>
+      <h2 className={classes.subTitle}>{PENALTIES_TITLE}</h2>
+      <Textarea size='lg' minRows={5} style={{width: '30%'}}/>
+      <h2 className={classes.subTitle}>{OTHER_TITLE}</h2>
+      <Textarea size='lg' minRows={5} style={{width: '30%'}}/>
       <NavigationButtons prevPath={PREV_PATH} nextPath={NEXT_PATH} />
     </div>
   );
