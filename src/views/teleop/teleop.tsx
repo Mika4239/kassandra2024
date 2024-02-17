3ed07200541a35186c1ac886f61ed9a1a6d69f0d
Mika <mikush.carmel@gmail.com>
2024-02-17 13:52:24 +0200

    feat(addRedux): added redux on teleop

--- src/views/teleop/teleop.tsx
+++ src/views/teleop/teleop.tsx
@@ -1,72 +1,131 @@
 import IconButton from "@mui/material/IconButton/IconButton";
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
 import useStyles from "./teleopStyles";
-import { useState } from "react";
 import NavigationButtons from "../../components/navigationButtons/navigationButtons";
+import { useAppSelector } from "../../redux/hooks";
+import { useDispatch } from "react-redux";
+import { setTeleopAmpFail, setTeleopAmpSuccess, setTeleopSpeakerFail, setTeleopSpeakerSuccess } from "../../redux/matchDataSlice";
 
-const TELEOP_TITLE = 'Teleop';
-const SPEAKER_TITLE = 'Speaker';
-const AMP_TITLE = 'Amp';
+const TELEOP_TITLE = "Teleop";
+const SPEAKER_TITLE = "Speaker";
+const AMP_TITLE = "Amp";
 
-const SUCCESS_TITLE = 'Success';
-const FAIL_TITLE = 'Fail';
+const SUCCESS_TITLE = "Success";
+const FAIL_TITLE = "Fail";
 
-const PREV_PATH = 'autonomous';
-const NEXT_PATH = 'endgame';
+const PREV_PATH = "autonomous";
+const NEXT_PATH = "endgame";
 
 const Teleop: React.FC = () => {
-    const { classes } = useStyles();
+  const { classes } = useStyles();
 
-    const [speakerCountSuccess, setSpeakerCountSuccess] = useState<number>(0);
-    const [ampCountSuccess, setAmpCountSuccess] = useState<number>(0);
-    const [speakerCountFail, setSpeakerCountFail] = useState<number>(0);
-    const [ampCountFail, setAmpCountFail] = useState<number>(0);
-    
-    return (
-        <div className={classes.teleopPage}>
-            <h1 className={classes.mainTitle}>{TELEOP_TITLE}</h1>
-            <h2 className={classes.subTitle}>{SPEAKER_TITLE}</h2>
-            <div className={classes.countButtons}>
-                <div className={classes.successButton}>
-                    <h3>{SUCCESS_TITLE}</h3>
-                    <div>
-                        <IconButton onClick={() => speakerCountSuccess > 0 && setSpeakerCountSuccess(prev => prev - 1)}><RemoveIcon/></IconButton>
-                        <>{speakerCountSuccess.toString()}</>
-                        <IconButton onClick={() => setSpeakerCountSuccess(prev => prev + 1)}><AddIcon/></IconButton>
-                    </div>
-                </div>
-                <div className={classes.failButton}>
-                    <h3>{FAIL_TITLE}</h3>
-                    <div>
-                        <IconButton onClick={() => speakerCountFail && setSpeakerCountFail(prev => prev - 1)}><RemoveIcon/></IconButton>
-                        <>{speakerCountFail.toString()}</>
-                        <IconButton onClick={() => setSpeakerCountFail(prev => prev + 1)}><AddIcon/></IconButton>
-                    </div>
-                </div>
-            </div>
-            <h2 className={classes.subTitle}>{AMP_TITLE}</h2>
-            <div className={classes.countButtons}>
-                <div className={classes.successButton}>
-                    <h3>{SUCCESS_TITLE}</h3>
-                    <div>
-                        <IconButton onClick={() => ampCountSuccess > 0 && setAmpCountSuccess(prev => prev - 1)}><RemoveIcon/></IconButton>
-                        <>{ampCountSuccess.toString()}</>
-                        <IconButton onClick={() => setAmpCountSuccess(prev => prev + 1)}><AddIcon/></IconButton>
-                    </div>
-                </div>
-                <div className={classes.failButton}>
-                    <h3>{FAIL_TITLE}</h3>
-                    <div>
-                        <IconButton onClick={() => ampCountFail > 0 && setAmpCountFail(prev => prev - 1)}><RemoveIcon/></IconButton>
-                        <>{ampCountFail.toString()}</>
-                        <IconButton onClick={() => setAmpCountFail(prev => prev + 1)}><AddIcon/></IconButton>
-                    </div>
-                </div>
-            </div>
-            <NavigationButtons prevPath={PREV_PATH} nextPath={NEXT_PATH}/>
+  const speakerSuccess = useAppSelector(
+    (state) => state.matchData.teleop.speaker.success
+  );
+  const speakerFail = useAppSelector(
+    (state) => state.matchData.teleop.speaker.fail
+  );
+  const ampSuccess = useAppSelector(
+    (state) => state.matchData.teleop.amp.success
+  );
+  const ampFail = useAppSelector(
+    (state) => state.matchData.teleop.amp.fail
+  );
+
+  const dispatch = useDispatch();
+
+  return (
+    <div className={classes.teleopPage}>
+      <h1 className={classes.mainTitle}>{TELEOP_TITLE}</h1>
+      <h2 className={classes.subTitle}>{SPEAKER_TITLE}</h2>
+      <div className={classes.countButtons}>
+        <div className={classes.successButton}>
+          <h3>{SUCCESS_TITLE}</h3>
+          <div>
+            <IconButton
+              onClick={() =>
+                speakerSuccess > 0 &&
+                dispatch(setTeleopSpeakerSuccess(speakerSuccess - 1))
+              }
+            >
+              <RemoveIcon />
+            </IconButton>
+            <>{speakerSuccess.toString()}</>
+            <IconButton
+              onClick={() =>
+                dispatch(setTeleopSpeakerSuccess(speakerSuccess + 1))
+              }
+            >
+              <AddIcon />
+            </IconButton>
+          </div>
         </div>
-    );
+        <div className={classes.failButton}>
+          <h3>{FAIL_TITLE}</h3>
+          <div>
+            <IconButton
+              onClick={() =>
+                speakerFail &&
+                dispatch(setTeleopSpeakerFail(speakerFail - 1))
+              }
+            >
+              <RemoveIcon />
+            </IconButton>
+            <>{speakerFail.toString()}</>
+            <IconButton
+              onClick={() =>
+                dispatch(setTeleopSpeakerFail(speakerFail + 1))
+              }
+            >
+              <AddIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+      <h2 className={classes.subTitle}>{AMP_TITLE}</h2>
+      <div className={classes.countButtons}>
+        <div className={classes.successButton}>
+          <h3>{SUCCESS_TITLE}</h3>
+          <div>
+            <IconButton
+              onClick={() =>
+                ampSuccess > 0 &&
+                dispatch(setTeleopAmpSuccess(ampSuccess - 1))
+              }
+            >
+              <RemoveIcon />
+            </IconButton>
+            <>{ampSuccess.toString()}</>
+            <IconButton
+              onClick={() => dispatch(setTeleopAmpSuccess(ampSuccess + 1))}
+            >
+              <AddIcon />
+            </IconButton>
+          </div>
+        </div>
+        <div className={classes.failButton}>
+          <h3>{FAIL_TITLE}</h3>
+          <div>
+            <IconButton
+              onClick={() =>
+                ampFail > 0 && dispatch(setTeleopAmpFail(ampFail - 1))
+              }
+            >
+              <RemoveIcon />
+            </IconButton>
+            <>{ampFail.toString()}</>
+            <IconButton
+              onClick={() => dispatch(setTeleopAmpFail(ampFail + 1))}
+            >
+              <AddIcon />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+      <NavigationButtons prevPath={PREV_PATH} nextPath={NEXT_PATH} />
+    </div>
+  );
 };
 
 export default Teleop;
