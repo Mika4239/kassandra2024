35bed456f8b4b2e9b553643aed26fd19ea8db0a0
Mika <mikush.carmel@gmail.com>
2024-02-17 16:36:02 +0200

    feat(addSigningDialogs): added sign up dialog

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -1,5 +1,7 @@
 import Button from "@mui/material/Button/Button";
 import useStyles from "./homeStyles";
+import SignUpDialog from "../../components/signUpDialog/signUpDialog";
+import { useState } from "react";
 
 const HOME_TITLE = 'Kassandra 2024';
 
@@ -9,12 +11,15 @@ const SIGN_UP_BUTTON = 'Sign Up';
 const Home = () => {
     const { classes } = useStyles();
 
+    const [open, setOpen] = useState<boolean>(false);
+
     return (
         <div className={classes.homePage}>
             <h1 className={classes.homeTitle}>{HOME_TITLE}</h1>
             <div>
                 <Button className={classes.signIn} variant="contained">{SIGN_IN_BUTTON}</Button>
-                <Button className={classes.signUp}>{SIGN_UP_BUTTON}</Button>
+                <Button className={classes.signUp} onClick={() => setOpen(true)}>{SIGN_UP_BUTTON}</Button>
+                <SignUpDialog open={open} setOpen={setOpen}/>
             </div>
         </div>
     );
