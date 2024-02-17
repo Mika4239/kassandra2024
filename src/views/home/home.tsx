d0364e7d7c1cc485bdeb634527c3f5243db814bd
Mika <mikush.carmel@gmail.com>
2024-02-17 16:48:50 +0200

    feat(addSigningDialogs): added sign in dialog

--- src/views/home/home.tsx
+++ src/views/home/home.tsx
@@ -2,27 +2,38 @@ import Button from "@mui/material/Button/Button";
 import useStyles from "./homeStyles";
 import SignUpDialog from "../../components/signUpDialog/signUpDialog";
 import { useState } from "react";
+import SignInDialog from "../../components/signInDialog/signInDialog";
 
-const HOME_TITLE = 'Kassandra 2024';
+const HOME_TITLE = "Kassandra 2024";
 
-const SIGN_IN_BUTTON = 'Sign In';
-const SIGN_UP_BUTTON = 'Sign Up';
+const SIGN_IN_BUTTON = "Sign In";
+const SIGN_UP_BUTTON = "Sign Up";
 
 const Home = () => {
-    const { classes } = useStyles();
+  const { classes } = useStyles();
 
-    const [open, setOpen] = useState<boolean>(false);
+  const [openSignIn, setOpenSignIn] = useState<boolean>(false);
+  const [openSignUp, setOpenSignUp] = useState<boolean>(false);
 
-    return (
-        <div className={classes.homePage}>
-            <h1 className={classes.homeTitle}>{HOME_TITLE}</h1>
-            <div>
-                <Button className={classes.signIn} variant="contained">{SIGN_IN_BUTTON}</Button>
-                <Button className={classes.signUp} onClick={() => setOpen(true)}>{SIGN_UP_BUTTON}</Button>
-                <SignUpDialog open={open} setOpen={setOpen}/>
-            </div>
-        </div>
-    );
+  return (
+    <div className={classes.homePage}>
+      <h1 className={classes.homeTitle}>{HOME_TITLE}</h1>
+      <div>
+        <Button
+          className={classes.signIn}
+          variant="contained"
+          onClick={() => setOpenSignIn(true)}
+        >
+          {SIGN_IN_BUTTON}
+        </Button>
+        <SignInDialog open={openSignIn} setOpen={setOpenSignIn} />
+        <Button className={classes.signUp} onClick={() => setOpenSignUp(true)}>
+          {SIGN_UP_BUTTON}
+        </Button>
+        <SignUpDialog open={openSignUp} setOpen={setOpenSignUp} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
