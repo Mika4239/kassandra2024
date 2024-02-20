1eb9c367102406a196c20ce7a1b1ddb50af7f654
Mika <mikush.carmel@gmail.com>
2024-02-20 17:52:31 +0200

    feat(addTeams): added other option on select match

--- src/components/selectFromData/selectFromData.tsx
+++ src/components/selectFromData/selectFromData.tsx
@@ -7,6 +7,8 @@ import { useAppSelector } from "../../redux/hooks";
 import { SelectMatchState } from "../../types/interfaces";
 import { useDispatch } from "react-redux";
 import { setMatchTeam } from "../../redux/matchDataSlice";
+import { TextField } from "@mui/material";
+import { useState } from "react";
 
 const SelectFromData: React.FC<SelectFromDataProps> = (props) => {
   const { classes } = useStyles();
@@ -17,14 +19,18 @@ const SelectFromData: React.FC<SelectFromDataProps> = (props) => {
   );
   const dispatch = useDispatch();
 
+  const [isOther, setIsOther] = useState<boolean>(data.find((item) => item === chosen) === undefined);
+
   return (
     <FormControl className={classes.selectBox}>
       <InputLabel id={name + "-label"}>{name}</InputLabel>
       <Select
+        required
         labelId={name + "-label"}
-        value={chosen}
+        value={isOther ? "other" : chosen}
         label={name}
         onChange={(e) => {
+          setIsOther(e.target.value === 'other');
           dispatch(setMatchTeam({ name: name, input: e.target.value }));
         }}
       >
@@ -33,7 +39,20 @@ const SelectFromData: React.FC<SelectFromDataProps> = (props) => {
             {dataTranslate(item, index)}
           </MenuItem>
         ))}
+        <MenuItem key="other" value="other">other</MenuItem>
       </Select>
+      {isOther &&
+        <TextField
+          label="Other Option"
+          name="otherOption"
+          value={chosen}
+          onChange={(e) => dispatch(setMatchTeam({ name: name, input: e.target.value }))}
+          variant="outlined"
+          fullWidth
+          margin="normal"
+          required
+        />
+      }
     </FormControl>
   );
 };
