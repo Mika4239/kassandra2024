e264d637eadf022d65995082360616d5b50e8ab3
Mika <mikush.carmel@gmail.com>
2024-03-05 08:39:08 +0200

    fix(cahngeToSucceeded); changed to succeeded

--- src/graphql/matchDataQueries.ts
+++ src/graphql/matchDataQueries.ts
@@ -41,7 +41,7 @@ export const getAllMatchData = `
                 stage
                 spotlit
                 trap{
-                    successed
+                    succeeded
                     tried
                 }
             }
